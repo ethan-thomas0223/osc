2e8ae48138cea586e9130cd62619bbe0c3b8b773
Ethan Thomas <thomasem@hendrix.edu>
2023-02-02 07:42:56 -0600

    start works

--- shell/src/bin/start.rs
+++ shell/src/bin/start.rs
@@ -1,17 +1,14 @@
 //Prints out the first ten lines of each file listed in the command-line arguments. 
 //If the first argument begins with a dash, use the number immediately following the dash instead of ten.
 //cargo run --bin start -15 filepath filepath
-
-
-
 use std::fs; 
 
 fn main() {
     //let paths = fs::read_dir("./").unwrap(); 
+
     
-    
-    let counter: i32 = 0;
-    let lcount: i32 = 10; 
+    let mut counter: i32 = 0;
+    let mut lcount: i32 = 10; 
     for arg in std::env::args().skip(1){
         counter += 1;
         if counter == 1{
@@ -21,8 +18,9 @@ fn main() {
                 lcount = num; 
             }
 
-        }else if counter > 1{
-            println!("{}", read_lines(lcount, &arg)); 
+        }else {
+            println!("Hi!");
+            read_lines(lcount, &arg).unwrap(); 
         }
     }
 
@@ -34,13 +32,13 @@ use std::fs::File;
 use std::io::BufReader; 
 use std::io; 
 
-fn read_lines<>(numLines: i32, filepath: &str) -> io::Result<()> {
+fn read_lines<>(numLines: i32, filepath: &str) -> std::io::Result<()> {
     let f = File::open(filepath)?;
     let f = BufReader::new(f);
-    let i = 0; 
+    let mut i = 0; 
     for line in f.lines() {
-        
-        if i != numLines{
+        println!("{i}");
+        if i < numLines{
             println!("{}", line.unwrap());
         }else{
             break; 
---
+++ shell/target/debug/start.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ethan/Drix/Comp\ Sci/Operating\ Systems/hw/osc/shell/target/debug/start: /mnt/c/Users/ethan/Drix/Comp\ Sci/Operating\ Systems/hw/osc/shell/src/bin/start.rs
