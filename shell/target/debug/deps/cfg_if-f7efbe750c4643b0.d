c049996ee939e76418c89d58bad040e5b66ba7ac
Ethan Thomas <thomasem@hendrix.edu>
2023-02-07 08:40:36 -0600

    cont

---
+++ shell/target/debug/deps/cfg_if-f7efbe750c4643b0.d
@@ -0,0 +1,7 @@
+/mnt/c/Users/ethan/Drix/Comp Sci/Operating Systems/hw/osc/shell/target/debug/deps/cfg_if-f7efbe750c4643b0.rmeta: /home/ethan_thomas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/mnt/c/Users/ethan/Drix/Comp Sci/Operating Systems/hw/osc/shell/target/debug/deps/libcfg_if-f7efbe750c4643b0.rlib: /home/ethan_thomas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/mnt/c/Users/ethan/Drix/Comp Sci/Operating Systems/hw/osc/shell/target/debug/deps/cfg_if-f7efbe750c4643b0.d: /home/ethan_thomas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/ethan_thomas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
