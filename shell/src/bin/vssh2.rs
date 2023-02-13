2d73d255fd1b3fa44384b6060150a1b805f190c6
Ethan Thomas <thomasem@hendrix.edu>
2023-02-13 16:52:06 -0600

    rework post meeting

--- shell/src/bin/vssh2.rs
+++ shell/src/bin/vssh2.rs
@@ -17,7 +17,16 @@ fn main() -> anyhow::Result<()> {
     
     if len(args) > 1{
         //meaning there would at least 2 args, therefore a pipe -> run the pipeline
-        pipeline(args);
+        match unsafe {fork()}? {
+            nix::unistd::ForkResult::Parent { child: _ } => {
+                println!("Continuing execution in parent process, new child has pid: {}", child);
+                waitpid(child, None).unwrap();
+                println!("Returned to parent - child is finished.");
+            }
+            nix::unistd::ForkResult::Child => {
+                pipeline(args);
+            }
+        }
     }else{
         //there's no pipe, so execute command as is 
         let cmd = externalize("{args[0]}");
@@ -36,41 +45,46 @@ fn pipeline(args: Vec<String>) -> anyhow::Result<()>{
     let mut curarg = len(args) - 1;
     let mut fd = 1; 
     for arg in args.rev() {
-        if curarg != 1{
-            let mut lhs: Vec<CString> = vec![args[curarg - 1].split_whitespace().as_c_str()?]
-        }else{
-            break;
-        }
-        let mut rhs: Vec<CString> = vec![arg.split_whitespace().as_c_str()?]; 
-        match unsafe {fork()}? {
-            nix::unistd::ForkResult::Parent { child } => {
-                println!("wc pid is {child}");
-                waitpid(child, None).unwrap();
-                println!("Finished!");
-            },
-            nix::unistd::ForkResult::Child => {
-                let (rhs_in, lhs_out) = pipe()?;
-                match unsafe {fork()}? {
-                    nix::unistd::ForkResult::Parent { child: _ } => {
-                        close(lhs_out)?;
-                        let flags: OFlag = [OFlag::O_CREAT, OFlag::O_WRONLY, OFlag::O_TRUNC].iter().copied().collect();
-                        let mode: Mode = [Mode::S_IRUSR, Mode::S_IWUSR].iter().copied().collect();
-                        //let file_out = open("wc.out", flags, mode)?;
-                        //dup2(file_out, 1)?;
-                        dup2(rhs_in, fd)?;
-                        dup2(lhs_out, fd)?;
-                        execvp(&rhs[0], &rhs)?;
-                    }
-                    nix::unistd::ForkResult::Child => {
-                        close(rhs_in)?;
-                        dup2(lhs_out, 1)?;
-                        //execvp(&lhs[0], &lhs)?;
+        if curarg > 1 {
+          
+            let mut rhs: Vec<CString> = vec![arg.split_whitespace().as_c_str()?]; 
+
+            match unsafe {fork()}? {
+                nix::unistd::ForkResult::Parent { child } => {
+                    println!("wc pid is {child}");
+                    waitpid(child, None).unwrap();
+                    println!("Finished!");
+                },
+                nix::unistd::ForkResult::Child => {
+                    let (rhs_in, lhs_out) = pipe()?;
+                    match unsafe {fork()}? {
+                        nix::unistd::ForkResult::Parent { child: _ } => {
+                            close(lhs_out)?;
+                            let flags: OFlag = [OFlag::O_CREAT, OFlag::O_WRONLY, OFlag::O_TRUNC].iter().copied().collect();
+                            let mode: Mode = [Mode::S_IRUSR, Mode::S_IWUSR].iter().copied().collect();
+                            //let file_out = open("wc.out", flags, mode)?;
+                            //dup2(file_out, 1)?;
+                            dup2(fd, 1)?;
+                            dup2(lhs_out, 0)?;
+                            execvp(&rhs[0], &rhs)?;
+                        }
+                        nix::unistd::ForkResult::Child => {
+                            close(rhs_in)?;
+                            fd = rhs_in; 
+                            //dup2(lhs_out, 1)?;
+                            //execvp(&lhs[0], &lhs)?;
+                        }
                     }
                 }
             }
         }
         curarg -= 1;
+        
     }
+    let nargs = externalize(args[0]);
+    dup2(fd, 1);
+    execvp(&nargs[0], &nargs);
+    
     Ok(())
 }
 
