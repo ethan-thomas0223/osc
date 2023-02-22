a849960bd51f3cc213252081cc1093955012b451
Ethan Thomas <thomasem@hendrix.edu>
2023-02-22 14:43:25 -0600

    Error fixed, normal bugs, one of many commits

--- shell/src/bin/webget.rs
+++ shell/src/bin/webget.rs
@@ -2,21 +2,22 @@ use openssl::ssl::{SslConnector, SslMethod};
 use std::io;
 use std::ffi::CString;
 use std::env;
+use std::io::Write; 
+use std::io::BufReader; 
+use std::io::BufRead;
+use std::net::TcpStream; 
 
 //Usage: webget url
 
 fn main() {
-    let mut args Vec<&str> = std::env::args().skip(1).split_whitespace().collect();
-    let mut url &str = args.last();
-    let mut cmd String = std::env::args().skip(1).to_string();
-    let cstring_cmd Vec<CString> = externalize(&cmd);
+    let mut url: String = std::env::args().last().unwrap();
     //got all my basic variables; Don't know what's next
 
     //parse to get message parts; skip over the https:// in url
-    let mut parts Vec<String> = url.skip(7).split("/");
-    let mut host String = parts[0].to_string();
-    let mut req = "";
-    for entry in parts.skip(1){
+    let mut parts = url.split("/").collect();
+    let mut host: String = parts[3].to_string();
+    let mut req = "".to_string();
+    for entry in parts.iter() {
         req += "/{entry}";
     }
     send_message(host, 443, req); 
@@ -32,7 +33,7 @@ fn send_message(host: &str, port: usize, message: &str) -> io::Result<()> {
     stream.write(message.as_bytes())?;
     //create buff reader on the stream 
     let buf = BufReader::new(stream);
-    let newmsg = "";
+    let newmsg = "".to_string();
     //iterate through buf reader using lines
     //add to sequence of string the we are getting back (print to clarify)
     //break up string to get header, shave it off, save the rest to local file
@@ -42,10 +43,4 @@ fn send_message(host: &str, port: usize, message: &str) -> io::Result<()> {
     println!("{}", newmsg); 
 
     Ok(())
-}
-
-fn externalize(command: &str) -> Vec<CString> {
-    command.split_whitespace()
-        .map(|s| CString::new(s).unwrap())
-        .collect()
 }
